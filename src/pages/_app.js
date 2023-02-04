80bab4a52fdc490563676d05bcc88a7ebe10dbe5
elgamlinfo <elgaml.info@gmail.com>
2023-02-04 21:46:36 +0200

    progress bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,8 +9,34 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { useRouter } from "next/router";
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import { useEffect } from "react";
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = (url) => {
+      console.log(`Loading: ${url}`)
+      NProgress.start()
+    }
+
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
   return <Component {...pageProps} />;
 }
